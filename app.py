44b19219635547d24ffda7f04783fafd8d41b357
Fabio <fabio.morais@amaro.com>
2019-09-03 22:01:52 -0300

    update files

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask_mysqldb import MySQL
 from flask import Flask, request, jsonify
-import yaml
 
 app = Flask(__name__)
-db = yaml.load(open('db.yaml'))
 
-app.config["MYSQL_HOST"] = db['mysql_host']
-app.config["MYSQL_USER"] = db['mysql_user']
-app.config["MYSQL_PASSWORD"] = db['mysql_password']
-app.config["MYSQL_DB"] = db['mysql_db']
+app.config["MYSQL_HOST"] = '##'
+app.config["MYSQL_USER"] = 'root'
+app.config["MYSQL_PASSWORD"] = '123123123'
+app.config["MYSQL_DB"] = 'flaskapp'
 
 mysql = MySQL(app)
 
--- get_rds_endpoint.sh
+++ get_rds_endpoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 REGION=us-east-1          #  change to your region
-pem="~/pem/mypem.pem" #  change to your .pem dir
+pem="~/pem/mypem.pem"     #  change to your .pem dir
 host=$(aws ec2 describe-instances --filters Name=tag:Name,Values=ansible-ec2 Name=instance-state-name,Values=running --query 'Reservations[0].Instances[0].[PublicIpAddress,Tags[?Key==``].Value[]]' --output text)
 user="ubuntu"
 project="simple-form"
 RDS=$(aws rds --region ${REGION} describe-db-instances --db-instance-identifier rds-webserver --query "DBInstances[*].Endpoint.Address" --output text )
 
 echo "stage change RDS"
-sed -i -f "s/##/${RDS}/g" db.yaml
+sed -i -f "s/##/${RDS}/g" app.py
 
 echo "copy db.yaml"
 scp -i ${pem} -r db.yaml ${user}@"${host}":${project}/
